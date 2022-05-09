5cf847e7a0fe9985f6550131bd5f5bfa3366c20a
MD027 <msdmohit27@gmail.com>
2022-05-09 07:11:02 +0530

    MVCAssignment4

---
+++ MVC5Assignment4/Models/MyDbcontext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+using System.Web.Configuration;
+
+namespace Assignment4.Models
+{
+    public class MyDbContext : DbContext
+    {
+        public MyDbContext() : base("name = conn")
+        {
+
+        }
+        public DbSet<Account> Accounts { get; set; }//to create table in db
+    }
+}
